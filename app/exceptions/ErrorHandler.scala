3d6ff9b73ac25418295bc4cbf66287e60b5b2397
David Santiago <d4v1x.sb@gmail.com>
2017-02-15 00:12:41 +0000

    added first version of a custom error handler.

---
+++ app/exceptions/ErrorHandler.scala
@@ -0,0 +1,26 @@
+package exceptions
+
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+
+/**
+  * Created by davidsantiago on 14/2/17.
+  */
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
